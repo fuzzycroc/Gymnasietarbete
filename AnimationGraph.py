b675c88a72ed521899d7ec7cd33635a0563ed0b9
fuzzycroc <152718375+fuzzycroc@users.noreply.github.com>
2023-12-06 13:41:29 +0100

    limits and animation
    Created new file for testing animation and added limits for x and y axis

--- AnimationGraph.py
+++ AnimationGraph.py
@@ -1,15 +1,20 @@
 #import libraries 
 import matplotlib.pyplot as plt 
 
-#Gives the star a x and y coordinates
-x =[0]
-y = [0]
+#Gives both stars x and y coordinates
+x =[2,-2]
+y = [2,-2]
 
 #Creates star
 plt.scatter(x, y, label ="test", color= "red", marker= "*", s=30)
 
+#Plots the x and y axis 
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
+
+#Creates limits of graph
+plt.xlim (-100, 100)
+plt.ylim (-100, 100)
 
 plt.show()
 
