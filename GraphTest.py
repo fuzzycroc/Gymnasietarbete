bdd9103f2efe2ff8bea611c69ee18a197f0b645d
fuzzycroc <152718375+fuzzycroc@users.noreply.github.com>
2023-12-06 13:32:46 +0100

    Scatter Chart Test
    Created a scatter chart of one star with x and y coordinates

---
+++ GraphTest.py
@@ -0,0 +1,14 @@
+#import libraries 
+import matplotlib.pyplot as plt 
+
+#Create x and y axis 
+x =[0]
+y = [0]
+
+plt.scatter(x, y, label ="test", color= "red", marker= "*", s=30)
+
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+
+plt.show()
+
