ca70f11281525d310a449c2e691cee0010568521
fuzzycroc <152718375+fuzzycroc@users.noreply.github.com>
2023-12-17 19:39:03 +0100

    Update Planetary_orbit.py

--- Main_files/Planetary_orbit.py
+++ Main_files/Planetary_orbit.py
@@ -4,11 +4,12 @@ import numpy as np
 
 
 time = 1000
-for i in range (100):
+for i in range (1000):
     time -= 1
 
 class Star: 
 
+    #bodies class
     def __init__(self, x, y, mass, x_vel, y_vel,  radius, marker, color):
         self.x = x 
         self.y = y
@@ -19,16 +20,26 @@ class Star:
         self.marker = marker 
         self.color = color 
 
+    #draw method
     def draw(self):
         plt.scatter(self.x, self.y, s = self.radius**2, c= self.color, marker = self.marker)
+    
+    def attraction(self):
+        G = 6.67408e-11 #N-m2/kg2
+        Au = 1,496e+11 #Au to m
+         
+
+
+
 
         
-
+#Creates bodies
 Star_1 = Star(1, 0, 20, 0, 0, 10, 'o','red')
 binary_Star_2 = Star(-1, 0, 20, 0, 0, 10, 'o', 'yellow')
-
 planet = Star(4, 4, 4, 0, 0, 14,'*', 'blue')
 
+
+#draws bodies
 Star_1.draw()
 binary_Star_2.draw()
 planet.draw()
